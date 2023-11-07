7cf5160b46f1e408a91e60aac8658b06d87fadee
dilettits <mantovani.d3@gmail.com>
2023-11-07 10:29:22 +0100

    voided last commit

--- palindrome.js
+++ palindrome.js
@@ -4,7 +4,9 @@ function isPalindrome(word) {
     let palindrome = '';
 
     for (let i = word.length - 1; i >= 0; i--) {
-        console.log(word[i]);
+        
+        stg = word[i];
+        palindrome += stg;
     }
 
     return palindrome;
